8ee297f7403fa16bef8bce553905b5cecbd41fee
Brad Huber <daybreaker@gmail.com>
2013-01-12 10:43:15 -0600

    Re-annotated

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: sections
+#
+#  id             :integer          not null, primary key
+#  section_number :integer
+#  x              :integer
+#  y              :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  chunk          :string(255)
+#
+
 class Section < ActiveRecord::Base
   attr_accessible :section_number, :x, :y
 end
