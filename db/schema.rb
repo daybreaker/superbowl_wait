7eb641dc0f430a3be0baadb83467cf9cb4f7e0fc
Brad Huber <daybreaker@gmail.com>
2013-01-12 10:20:35 -0600

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112071655) do
+ActiveRecord::Schema.define(:version => 20130112080319) do
 
   create_table "destinations", :force => true do |t|
     t.string   "destination_type"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(:version => 20130112071655) do
   end
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+
+  create_table "sections", :force => true do |t|
+    t.integer  "section_number"
+    t.integer  "x"
+    t.integer  "y"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
