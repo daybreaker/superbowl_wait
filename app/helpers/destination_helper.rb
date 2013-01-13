ba50ff2ad3456336981afe76a6fa89bf71624252
Brad Huber <daybreaker@gmail.com>
2013-01-13 00:39:45 -0600

    Showing destinations

--- app/helpers/destination_helper.rb
+++ app/helpers/destination_helper.rb
@@ -1,2 +1,11 @@
 module DestinationHelper
+  def scale_x(point)
+    # 15px to offset width and center image
+    point * Section::SUPERDOME_IMAGE[:width] / 100 - 15
+  end
+  
+  def scale_y(point)
+    # 15px to offset height and center image
+    point * Section::SUPERDOME_IMAGE[:height] / 100 - 15
+  end
 end
